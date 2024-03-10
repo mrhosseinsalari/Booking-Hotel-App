8ff3692bc2d07d81e8ac625a9cc4aeb07abdacef
Hossein Salari <hosein.salari81@gmail.com>
2024-03-10 18:04:51 +0330

    Hotels provider created

---
+++ src/components/context/HotelsProvider.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { createContext } from "react";
+import { useSearchParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+
+const HotelContext = createContext();
+
+function HotelsProvider({ children }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const destination = searchParams.get("destination");
+  const room = JSON.parse(searchParams.get("options"))?.room;
+
+  // name_like, host_location_like => _like
+  const { isLoading, data: hotels } = useFetch(
+    "http://localhost:5000/hotels",
+    `q=${destination || ""}&accommodates_gte=${room || 1}`
+  );
+
+  return (
+    <HotelContext.Provider value={{ isLoading, hotels }}>
+      {children}
+    </HotelContext.Provider>
+  );
+}
+
+export default HotelsProvider;
+
+export function useHotels() {
+  return useContext(HotelContext);
+}
