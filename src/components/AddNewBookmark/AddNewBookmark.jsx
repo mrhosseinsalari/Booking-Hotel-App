ce09214f94d601be33b48a65612561c4ca72d3ad
Hossein Salari <hosein.salari81@gmail.com>
2024-03-14 16:04:13 +0330

    Fetch location data based on latitude and longitude in add bookmark page

--- src/components/AddNewBookmark/AddNewBookmark.jsx
+++ src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,14 +1,59 @@
 import { useNavigate } from "react-router-dom";
 import useUrlLocation from "../../hooks/useUrlLocation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loader from "../Loader/Loader";
+import ReactCountryFlag from "react-country-flag";
+
+const BASE_GEOCODING_URL =
+  "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function AddNewBookmark() {
   const navigate = useNavigate();
   const [lat, lng] = useUrlLocation();
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [isLoadingGeoCoding, setIsLoadingGeoCoding] = useState(false);
+  const [geoCodingError, setGeoCodingError] = useState(null);
+
+  useEffect(() => {
+    if (!lat || !lng) return;
+
+    async function fetchLocationData() {
+      setIsLoadingGeoCoding(true);
+      setGeoCodingError(null);
+
+      try {
+        const { data } = await axios.get(
+          `${BASE_GEOCODING_URL}?latitude=${lat}&longitude=${lng}`
+        );
+
+        if (!data.countryCode)
+          throw new Error(
+            "This location is not a city! please click somewhere else."
+          );
+
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+        setCountryCode(data.countryCode);
+      } catch (error) {
+        setGeoCodingError(error.message);
+      } finally {
+        setIsLoadingGeoCoding(false);
+      }
+    }
+
+    fetchLocationData();
+  }, [lat, lng]);
 
   const handleBack = (e) => {
     e.preventDefault();
     navigate(-1);
   };
+
+  if (isLoadingGeoCoding) return <Loader />;
+  if (geoCodingError) return <p>{geoCodingError}</p>;
 
   return (
     <div className="addNewBookmark">
@@ -16,11 +61,26 @@ function AddNewBookmark() {
       <form className="form">
         <div className="formControl">
           <label htmlFor="cityName">CityName</label>
-          <input type="text" name="cityName" id="cityName" />
+          <input
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            type="text"
+            name="cityName"
+            id="cityName"
+          />
         </div>
         <div className="formControl">
           <label htmlFor="country">Country</label>
-          <input type="text" name="country" id="country" />
+          <input
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            type="text"
+            name="country"
+            id="country"
+          />
+          {countryCode && (
+            <ReactCountryFlag className="flag" svg countryCode={countryCode} />
+          )}
         </div>
         <div className="buttons">
           <button onClick={handleBack} className="btn btn--back">
